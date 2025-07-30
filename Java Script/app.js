f79610cf374a794a63cfaff144d244f137a56248
manishmeher <manish.meher774623@gmail.com>
2025-07-30 17:52:07 +0530

    update delta 7.0

--- Java Script/app.js
+++ Java Script/app.js
@@ -596,17 +596,54 @@
 
 // demo();
 
-function one() {
-    return 1;
-}
+// function one() {
+//     return 1;
+// }
 
-function two() {
-    return one() + one();
-}
+// function two() {
+//     return one() + one();
+// }
 
-function three() {
-    let ans = two() + one();
-    console.log(ans);
-}
+// function three() {
+//     let ans = two() + one();
+//     console.log(ans);
+// }
 
-three();
+// three();
+
+//API Request
+
+let url = "https://catfact.ninja/fact";
+
+// fetch(url)
+// .then((res) => {
+//     return res.json();
+// })
+// .then((data) => {
+//     console.log("data 1 = ", data.fact);
+//     return fetch(url);
+// })
+// .then((res) => {
+//     return res.json();
+// })
+// .then((data2) => {
+//     console.log("data 2 =", data2.fact);
+// })
+// .catch((err) => {
+//     console.log("ERROR-", err);
+// });
+
+async function getFacts() {
+    try {
+        let res = await fetch(url);
+        let data = await res.json();
+        console.log(data.fact);
+
+        let res2 = await fetch(url);
+        let data2 = await res2.json();
+        console.log(data2.fact);
+    } catch(e) {
+        console.log("error -", e);
+    }
+    console.log("bye");
+}
